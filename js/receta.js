4ea2b73bc6072e7f24a5da0816f817866291c7e4
FelipeCapilla <fcapilla@udesa.edu.ar>
2024-11-08 12:49:24 -0300

    wip

---
+++ js/receta.js
@@ -0,0 +1,30 @@
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString)
+let id = queryStringObj.get("id")
+console.log(id)
+
+const receta = document.querySelector(".receta")
+let receta_Seleccionada = ""
+
+fetch(`https://dummyjson.com/recipes/${id}`)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+  console.log(data);
+  
+    let markUp = `
+            <article>
+                <h1>${data.name}</h1>
+                <p>Instrucciones: ${data.instructions}</p>
+                <p>Tiempo de cocción: ${data.prepTimeMinutes}</p>          
+                <img src=${data.image} alt= ${data.name}>
+                <p>Categorías: ${data.mealType}</p>
+            </article>
+            `;
+           
+            receta.innerHTML = markUp
+})
+.catch(function(error) {
+  console.log("Error: ", error);
+})
