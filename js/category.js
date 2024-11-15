81d317fdc36b6ab82e1c25f897d1eb1b4a08967b
FelipeCapilla <fcapilla@udesa.edu.ar>
2024-11-15 10:19:54 -0300

    wip

--- js/category.js
+++ js/category.js
@@ -5,20 +5,21 @@ let cat = queryStringObj.get("cat")
 const category = document.querySelector(".category")
 let categoria = ""
 
-fetch('https://dummyjson.com/recipes/tag/Pakistani')
+fetch(`https://dummyjson.com/recipes/tag/${cat}`)
 .then(function(response) {
   return response.json()
 })
 .then(function(data) {
   console.log(data);
   for(let i=0; i < data.recipes.length; i++){
-    const cat = data.recipes[i]
+    const cate = data.recipes[i]
     let markUp = `
+        <h1>${cat}</h1>
         <article>
-            <img src=${cat.image} alt= ${cat.name}>
-            <p>${cat.name}</p>
-            <p>Nivel de dificultad: ${cat.difficulty}</p>
-            <a href=receta.html?id=${cat.id}> Ir al detalle </a>
+            <img src=${cate.image} alt= ${cate.name}>
+            <p>${cate.name}</p>
+            <p>Nivel de dificultad: ${cate.difficulty}</p>
+            <a href=receta.html?id=${cate.id}> Ir al detalle </a>
         </article>
         `;
         categoria += markUp
