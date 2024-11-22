ffc40ba26b33dbb7de9e91da60d03de00045dbed
sarnicola <francosarnicola@MacBook-Pro-de-Franco.local>
2024-11-22 12:13:56 -0300

    login y register

--- js/login.js
+++ js/login.js
@@ -1,29 +1,30 @@
-const email = document.getElementById ('email');
-const password = document.getElementById ('password');
+const form = document.querySelector('.login_form')
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
 const invalidEmail =document.querySelector ('.email ');
-const invalidPassword = document.getElementById ('.password');
+const invalidPassword = document.querySelector ('.password');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    let errors = false ;
-
+    let errors = false;
+    console.log('erro', errors)
     if (email.value === '') {
-        invalidEmail.innerText = 'no se completo el email';
+        invalidEmail.innerText = 'No se completo el email';
         invalidEmail.style.display = 'block';
         errors = true;
     } else {
         invalidEmail.style.display = 'none';
     }
     if (password.value === '') {
-        invalidPassword.innerText = 'no se completo la contraseña';
+        invalidPassword.innerText = 'No se completo la contraseña';
         invalidPassword.style.display = 'block';
         errors = true;
     } else {
         invalidEmail.style.display = 'none';
     }
     if (!errors) {
-        this.submit ();
+        this.submit();
     }
 });
