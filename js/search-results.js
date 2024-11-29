be3ae0a0eae7b188fda8a7520fcb6767fa600b74
Jack Guelman <jackguelman@MacBook-Air-de-Jack-2.local>
2024-11-29 10:20:44 -0300

    sear-results

--- js/search-results.js
+++ js/search-results.js
@@ -1,19 +1,23 @@
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString)
-let cat = queryStringObj.get("q")
+let bus = queryStringObj.get("search")
 
-const category = document.querySelector(".category")
-const titulo = document.querySelector(".titulo")
-let categoria = ""
+let seccion = document.querySelector(".search")
+let titulo = document.querySelector(".titulo")
 
-fetch('https://dummyjson.com/recipes/search')
+
+
+console.log(bus)
+fetch(`https://dummyjson.com/recipes/search?q=${bus}`)
 .then(function(response) {
   return response.json()
 })
 .then(function(data) {
-  console.log(data);
+    console.log(data)
+  
   for(let i=0; i < data.recipes.length; i++){
-    const cate = data.recipes[i]
+    let cate = data.recipes[i]
+    console.log(cate)
     let markUp = `
         <article>
             <img src=${cate.image} alt= ${cate.name}>
@@ -22,10 +26,9 @@ fetch('https://dummyjson.com/recipes/search')
             <a href=receta.html?id=${cate.id}> Ir al detalle </a>
         </article>
         `;
-        categoria += markUp
+        seccion.innerHTML += markUp
     }
-    category.innerHTML = categoria;  
-    titulo.innerText = cat
+    
 })
 .catch(function(error) {
   console.log("Error: ", error);
